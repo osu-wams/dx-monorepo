0e84feff6e030c0ecf610b8e488e6c4657af1126
Josh Gum <revgum@gmail.com>
2020-01-10 07:35:57 -0800

    adds basic RN app with Expo

---
+++ packages/apps/dx-mobile/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './AppStyles';
+
+export default function App(): JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
