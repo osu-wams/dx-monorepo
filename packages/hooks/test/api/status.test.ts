1b36c9469c52c772d7032f97a0cfba965adbcbcb
Josh Gum <revgum@gmail.com>
2020-01-20 10:34:59 -0800

    reworks status tests to test the hook

--- packages/hooks/test/api/status.test.ts
+++ packages/hooks/test/api/status.test.ts
@@ -1,21 +1,23 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { getStatus, allOperational, sortedByStatus, withStickyIncidents } from '../../src/api/status';
-import statusData from '../../src/mocks/status';
+import { renderHook } from '@testing-library/react-hooks';
+import { useStatus, mockStatus, sortedByStatus, allOperational, withStickyIncidents } from '../../src/api/status';
 
 const mock = new MockAdapter(axios);
 
-describe('getStatus', () => {
+describe('useStatus', () => {
   it('gets all statuses', async () => {
-    mock.onGet('/api/status').reply(200, statusData.data);
-    const result = await getStatus();
-    expect(result.map(c => c.id)).toStrictEqual(statusData.data.map(c => c.id));
+    mock.onGet('/api/status').reply(200, mockStatus.data);
+    const { result, waitForNextUpdate } = renderHook(() => useStatus());
+    await waitForNextUpdate();
+    expect(result.current.data).toEqual(mockStatus.data);
   });
 
   it('sorts the components based on the status', async () => {
-    mock.onGet('/api/status').reply(200, statusData.data);
-    const result = await getStatus();
-    expect(sortedByStatus(result).map(c => c.statusText)).toStrictEqual([
+    mock.onGet('/api/status').reply(200, mockStatus.data);
+    const { result, waitForNextUpdate } = renderHook(() => useStatus());
+    await waitForNextUpdate();
+    expect(sortedByStatus(result.current.data).map(c => c.statusText)).toStrictEqual([
       'Major Outage',
       'Performance Issues',
       'Operational',
@@ -23,29 +25,33 @@ describe('getStatus', () => {
     ]);
   });
   it('returns that some components have non operational status', async () => {
-    mock.onGet('/api/status').reply(200, statusData.data);
-    const result = await getStatus();
-    expect(allOperational(result)).toBeFalsy();
+    mock.onGet('/api/status').reply(200, mockStatus.data);
+    const { result, waitForNextUpdate } = renderHook(() => useStatus());
+    await waitForNextUpdate();
+    expect(allOperational(result.current.data)).toBeFalsy();
   });
   it('returns that all components are operational', async () => {
     mock.onGet('/api/status').reply(
       200,
-      statusData.data.map(c => ({ ...c, status: 1 })),
+      mockStatus.data.map(c => ({ ...c, status: 1 })),
     );
-    const result = await getStatus();
-    expect(allOperational(result)).toBeTruthy();
+    const { result, waitForNextUpdate } = renderHook(() => useStatus());
+    await waitForNextUpdate();
+    expect(allOperational(result.current.data)).toBeTruthy();
   });
   it('returns components with sticky incidents', async () => {
-    mock.onGet('/api/status').reply(200, statusData.data);
-    const result = await getStatus();
-    expect(withStickyIncidents(result)).toHaveLength(1);
+    mock.onGet('/api/status').reply(200, mockStatus.data);
+    const { result, waitForNextUpdate } = renderHook(() => useStatus());
+    await waitForNextUpdate();
+    expect(withStickyIncidents(result.current.data)).toHaveLength(1);
   });
   it('returns no components with sticky incidents', async () => {
     mock.onGet('/api/status').reply(
       200,
-      statusData.data.map(c => ({ ...c, incidents: [] })),
+      mockStatus.data.map(c => ({ ...c, incidents: [] })),
     );
-    const result = await getStatus();
-    expect(withStickyIncidents(result)).toHaveLength(0);
+    const { result, waitForNextUpdate } = renderHook(() => useStatus());
+    await waitForNextUpdate();
+    expect(withStickyIncidents(result.current.data)).toHaveLength(0);
   });
 });
