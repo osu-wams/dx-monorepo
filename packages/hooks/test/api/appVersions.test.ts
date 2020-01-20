e4662b8873a6fe974598e4e073851b032b3fff2b
Josh Gum <revgum@gmail.com>
2020-01-20 10:26:13 -0800

    reworks appversions test to test the hook

--- packages/hooks/test/api/appVersions.test.ts
+++ packages/hooks/test/api/appVersions.test.ts
@@ -1,24 +1,31 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { getAppVersions } from '../../src/api/appVersions';
+import { renderHook } from '@testing-library/react-hooks';
+import { useAppVersions } from '../../src/api/appVersions';
 
 const mock = new MockAdapter(axios);
 
-describe('getAppVersions', () => {
+describe('useAppVersions', () => {
   it('gets versions on successful returns', async () => {
     mock.onGet('/healthcheck').reply(200, { version: 'server-tested-version' });
     mock.onGet('/app_version').reply(200, 'client-tested-version');
-    const result = await getAppVersions();
-    expect(result).toEqual({
+    const { result, waitForNextUpdate } = renderHook(() => useAppVersions({ serverVersion: '', appVersion: '' }));
+    await waitForNextUpdate();
+    expect(result.current.loading).toBeFalsy();
+    expect(result.current.error).toBeFalsy();
+    expect(result.current.data).toEqual({
       serverVersion: 'server-tested-version',
       appVersion: 'client-tested-version',
     });
   });
-  it('falls back gracefully when requests fail', async () => {
+  it('handles api errors with default data', async () => {
     mock.onGet('/healthcheck').reply(500);
     mock.onGet('/app_version').reply(500);
-    const result = await getAppVersions();
-    expect(result).toEqual({
+    const { result, waitForNextUpdate } = renderHook(() => useAppVersions({ serverVersion: '', appVersion: '' }));
+    await waitForNextUpdate();
+    expect(result.current.loading).toBeFalsy();
+    expect(result.current.error).toBeFalsy();
+    expect(result.current.data).toEqual({
       serverVersion: 'failed-to-fetch',
       appVersion: 'failed-to-fetch',
     });
