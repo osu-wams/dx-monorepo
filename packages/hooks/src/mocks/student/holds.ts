7310577f4b3847b95aeb61c1e2bc561c96890d32
Josh Gum <revgum@gmail.com>
2020-01-22 15:06:49 -0800

    fixes holds mock data

--- packages/hooks/src/mocks/student/holds.ts
+++ packages/hooks/src/mocks/student/holds.ts
@@ -1,21 +1,7 @@
 export default {
   data: [
     {
-      id: '123-BOGUS-ID',
-      type: 'holds',
-      attributes: {
-        holds: [
-          {
-            description: 'Bill is overdue',
-            fromDate: '2019-12-31',
-            reason: 'CS 161',
-            toDate: '2020-12-31',
-          },
-        ],
-      },
-      links: {
-        self: null,
-      },
+      description: 'Bill is overdue',
     },
   ],
   loading: false,
