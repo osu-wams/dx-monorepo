b502225bf6dc5a70fe508c4815d74261c1f512b4
Josh Gum <revgum@gmail.com>
2020-05-02 07:41:56 -0700

    fixes broken tests

--- packages/hooks/src/mocks/alerts.ts
+++ packages/hooks/src/mocks/alerts.ts
@@ -2,13 +2,13 @@ export const raveAlerts = {
   data: [
     {
       title: 'First Rave',
-      date: new Date(),
+      date: new Date().toString(),
       content: 'Raving bruh',
       type: 'rave',
     },
     {
       title: 'Second Rave',
-      date: new Date(),
+      date: new Date().toString(),
       content: 'Raving bruh',
       type: 'rave',
     },
@@ -21,13 +21,13 @@ export const dxAlerts = {
   data: [
     {
       title: 'BobRoss',
-      date: new Date(),
+      date: new Date().toString(),
       content: 'Lives in an amongst the happy little accidents.',
       type: 'info',
     },
     {
       title: 'Old Dx Alert',
-      date: new Date(),
+      date: new Date().toString(),
       content: 'Raving bruh',
       type: 'warn',
     },
