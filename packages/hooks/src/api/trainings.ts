a7c675c1e1f094715a7b89eba63b085b4f02134b
Josh Gum <revgum@gmail.com>
2020-07-01 11:12:03 -0700

    adds hook, default configurations, and test coverage

--- packages/hooks/src/api/trainings.ts
+++ packages/hooks/src/api/trainings.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
+import { useQuery, BaseQueryOptions, QueryResult } from 'react-query';
 import mock from '../mocks/trainings';
 import { Types } from '@osu-wams/lib';
+import { REACT_QUERY_DEFAULT_CONFIG } from '../constants';
 
 export const mockTrainings = mock;
 
 export const getTrainings = (): Promise<Types.Training[]> => axios.get(`/api/trainings`).then(res => res.data);
+
+export const useTrainings = (
+  opts: BaseQueryOptions = REACT_QUERY_DEFAULT_CONFIG,
+): QueryResult<Types.Training[], Error> => useQuery('trainings', getTrainings, opts);
---
+++ packages/hooks/src/constants.ts
@@ -0,0 +1,29 @@
+import { BaseQueryOptions } from 'react-query';
+
+const REACT_QUERY_DEFAULT_CONFIG: BaseQueryOptions = {
+  /**
+   * Set this to `false` to disable automatic refetching when the query mounts or changes query keys.
+   * To refetch the query, use the `refetch` method returned from the `useQuery` instance.
+   */
+  enabled: true,
+  /**
+   * Query results that are currently rendered on the screen (via useQuery and similar hooks) will become
+   * "stale" immediately after they are resolved and will be refetched automatically in the background when
+   * they are rendered or used again. To change this, you can alter the default staleTime for queries
+   * to something other than 0 milliseconds.
+   */
+  staleTime: 1000 * 60 * 5,
+  /**
+   * Query results that become unused (all instances of the query are unmounted) will still be cached
+   * in case they are used again for a default of 5 minutes before they are garbage collected. To change
+   * this, you can alter the default cacheTime for queries to something other than 1000 * 60 * 5 milliseconds.
+   */
+  cacheTime: 1000 * 60 * 10,
+  /**
+   * Stale queries will automatically be refetched in the background when the browser window is refocused
+   * by the user. You can disable this using the refetchOnWindowFocus option in queries or the global config.
+   */
+  refetchOnWindowFocus: true,
+};
+
+export { REACT_QUERY_DEFAULT_CONFIG };
