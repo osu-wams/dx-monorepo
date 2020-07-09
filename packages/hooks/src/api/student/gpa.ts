4b2d41f475168815b2791e7f4a4d8d06341c6f73
Josh Gum <revgum@gmail.com>
2020-07-09 13:58:02 -0700

    adds gpa level to the lib types

--- packages/hooks/src/api/student/gpa.ts
+++ packages/hooks/src/api/student/gpa.ts
@@ -1,20 +1,15 @@
 import axios from 'axios';
 import useAPICall from '../../useAPICall';
 import mocks from '../../mocks/student/gpa';
+import { Types } from '@osu-wams/lib';
 
 export const mockGpa = mocks;
 
-export const gpaInitialState: GpaLevel[] = [{ gpa: '', gpaType: '', level: '' }];
-export const getGpa = (): Promise<GpaLevel> => axios.get(`/api/student/gpa`).then(res => res.data);
+export const gpaInitialState: Types.GpaLevel[] = [{ gpa: '', gpaType: '', level: '' }];
+export const getGpa = (): Promise<Types.GpaLevel> => axios.get(`/api/student/gpa`).then(res => res.data);
 export const useGpa = () =>
-  useAPICall<GpaLevel[]>({
+  useAPICall<Types.GpaLevel[]>({
     api: getGpa,
-    dataTransform: (data: GpaLevel[]) => data,
+    dataTransform: (data: Types.GpaLevel[]) => data,
     initialState: gpaInitialState,
   });
-
-export type GpaLevel = {
-  gpa: string;
-  gpaType: string;
-  level: string;
-};
