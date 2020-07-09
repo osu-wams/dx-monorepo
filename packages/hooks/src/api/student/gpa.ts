1f2dbe0f9fe1189321c14916be0ebac02fb7b430
Josh Gum <revgum@gmail.com>
2020-07-09 13:59:34 -0700

    adds levelCode to initial state

--- packages/hooks/src/api/student/gpa.ts
+++ packages/hooks/src/api/student/gpa.ts
@@ -5,7 +5,7 @@ import { Types } from '@osu-wams/lib';
 
 export const mockGpa = mocks;
 
-export const gpaInitialState: Types.GpaLevel[] = [{ gpa: '', gpaType: '', level: '' }];
+export const gpaInitialState: Types.GpaLevel[] = [{ gpa: '', gpaType: '', level: '', levelCode: '' }];
 export const getGpa = (): Promise<Types.GpaLevel> => axios.get(`/api/student/gpa`).then(res => res.data);
 export const useGpa = () =>
   useAPICall<Types.GpaLevel[]>({
