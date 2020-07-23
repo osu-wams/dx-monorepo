44e8ca4f93d734285ca0188b74af17f3d24cc574
Josh Gum <revgum@gmail.com>
2020-07-23 11:15:31 -0700

    updates planner-items api call to use react-query

--- packages/hooks/test/api/student/plannerItems.test.ts
+++ packages/hooks/test/api/student/plannerItems.test.ts
@@ -1,29 +1,42 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import { renderHook } from '@testing-library/react-hooks';
-import { usePlannerItems, mockPlannerItems } from '../../../src/api/student/plannerItems';
+import { usePlannerItems, mockPlannerItems, getPlannerItems } from '../../../src/api/student/plannerItems';
+import { queryCache } from 'react-query';
 
 const mock = new MockAdapter(axios);
+
+afterEach(() => {
+  queryCache.clear();
+  mock.reset();
+});
+describe('getPlannerItems', () => {
+  it('gets trainings on successful returns', async () => {
+    mock.onGet('/api/student/planner-items').replyOnce(200, mockPlannerItems.data);
+    const result = await getPlannerItems();
+    expect(result).toEqual(mockPlannerItems.data);
+  });
+  it('handles api error', async () => {
+    mock.onGet('/api/student/planner-items').replyOnce(500);
+    await getPlannerItems().catch(err => expect(err.message).toEqual('Request failed with status code 500'));
+  });
+});
 
 describe('usePlannerItems', () => {
   it('gets planner items on successful returns', async () => {
     mock.onGet('/api/student/planner-items').reply(200, mockPlannerItems.data);
-    const { result, waitForNextUpdate } = renderHook(() =>
-      usePlannerItems(() => console.log('test called error callback')),
-    );
+    const { result, waitForNextUpdate } = renderHook(() => usePlannerItems());
+    expect(result.current.isLoading).toBeTruthy();
     await waitForNextUpdate();
-    expect(result.current.loading).toBeFalsy();
-    expect(result.current.error).toBeFalsy();
+    expect(result.current.isLoading).toBeFalsy();
     expect(result.current.data).toEqual(mockPlannerItems.data);
   });
   it('handles api error', async () => {
-    mock.onGet('/api/student/planner-items').reply(500);
-    const { result, waitForNextUpdate } = renderHook(() =>
-      usePlannerItems(() => console.log('test called error callback')),
-    );
+    mock.onGet('/api/student/planner-items').reply(500, '');
+    const { result, waitForNextUpdate } = renderHook(() => usePlannerItems());
+    expect(result.current.isLoading).toBeTruthy();
+    expect(result.current.isError).toBeFalsy();
     await waitForNextUpdate();
-    expect(result.current.loading).toBeFalsy();
-    expect(result.current.error).toBeTruthy();
-    expect(result.current.data).toEqual([]);
+    expect(result.current.failureCount).toBe(1);
   });
 });
