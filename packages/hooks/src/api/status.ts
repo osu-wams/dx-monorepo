d8d7704a39f491a93cc32217d5470149c259dc30
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2020-10-16 08:21:03 -0700

    Updates status dests and helper methods to deal with undefined/return empty data

--- packages/hooks/src/api/status.ts
+++ packages/hooks/src/api/status.ts
@@ -17,17 +17,19 @@ export const STATUS_SORT: { [k: number]: number } = {
  * This is why the sort key is generated with the status and name so that natural lexigraphical sorting works.
  * @param components the IT System components from the API
  */
-export const sortedByStatus = (components: ICachetComponent[]): ICachetComponent[] => {
-  return components
-    ?.map(c => ({ ...c, key: `${STATUS_SORT[c.status]}${c.name}` }))
-    .sort((a, b) => (a.key > b.key ? 1 : -1));
+export const sortedByStatus = (components?: ICachetComponent[]): ICachetComponent[] => {
+  return (
+    components
+      ?.map(c => ({ ...c, key: `${STATUS_SORT[c.status]}${c.name}` }))
+      .sort((a, b) => (a.key > b.key ? 1 : -1)) ?? []
+  );
 };
 
 /**
  * Return a list of components having incidents for the Sticky Incident portion of the UI
  * @param components the IT System components from the API
  */
-export const withStickyIncidents = (components: ICachetComponent[]): ICachetComponent[] => {
+export const withStickyIncidents = (components?: ICachetComponent[]): ICachetComponent[] => {
   return components?.filter(c => c.incidents.length > 0) ?? [];
 };
 
@@ -35,7 +37,7 @@ export const withStickyIncidents = (components: ICachetComponent[]): ICachetComp
  * Return whether all components are in the Operational status.
  * @param components the IT System components from the API
  */
-export const allOperational = (components: ICachetComponent[]): boolean => {
+export const allOperational = (components?: ICachetComponent[]): boolean => {
   return components?.filter(c => c.status > 1).length === 0 ?? false;
 };
 
