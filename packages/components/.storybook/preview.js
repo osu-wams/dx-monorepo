97228deb4570bed37d12d2cc3004b8caf962329c
Josh Gum <revgum@gmail.com>
2020-11-04 14:24:31 -0800

    uses src stories and decorator for theme

--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -1,4 +1,15 @@
+import React from 'react';
+import { light } from '../src/theme/themes';
+import { ThemeProvider } from 'styled-components';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+export const decorators = [
+  Story => (
+    <ThemeProvider theme={light}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
