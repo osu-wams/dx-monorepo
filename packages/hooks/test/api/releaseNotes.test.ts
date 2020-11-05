91da21cd7369aed54a34aad8fc055580c1973cbc
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2020-11-05 08:35:43 -0800

    releaseNotes now in react-query  and additional tests

--- packages/hooks/test/api/releaseNotes.test.ts
+++ packages/hooks/test/api/releaseNotes.test.ts
@@ -1,25 +1,48 @@
+import { getReleaseNotes } from './../../src/api/releaseNotes';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import { renderHook } from '@testing-library/react-hooks';
 import { useReleaseNotes, mockReleaseNotes } from '../../src/api/releaseNotes';
+import { queryCache } from 'react-query';
 
 const mock = new MockAdapter(axios);
 
-describe('useReleaseNotes', () => {
-  it('gets page contents on successful returns', async () => {
-    mock.onGet('/api/release-notes').reply(200, mockReleaseNotes.data);
-    const { result, waitForNextUpdate } = renderHook(() => useReleaseNotes());
-    await waitForNextUpdate();
-    expect(result.current.loading).toBeFalsy();
-    expect(result.current.error).toBeFalsy();
-    expect(result.current.data).toEqual(mockReleaseNotes.data);
+const api = '/api/release-notes';
+
+afterEach(() => {
+  queryCache.clear();
+  mock.reset();
+});
+
+describe('getReleaseNotes', () => {
+  it('gets gpa on successful returns', async () => {
+    mock.onGet(api).replyOnce(200, mockReleaseNotes.releaseNotesData);
+    const result = await getReleaseNotes();
+    expect(result).toEqual(mockReleaseNotes.releaseNotesData);
   });
   it('handles api error', async () => {
-    mock.onGet('/api/release-notes').reply(500);
+    mock.onGet(api).replyOnce(500);
+    await getReleaseNotes().catch(err => expect(err.message).toEqual('Request failed with status code 500'));
+  });
+});
+
+describe('useReleaseNotes', () => {
+  it('gets page contents on successful returns', async () => {
+    mock.onGet(api).reply(200, mockReleaseNotes.releaseNotesData);
     const { result, waitForNextUpdate } = renderHook(() => useReleaseNotes());
     await waitForNextUpdate();
-    expect(result.current.loading).toBeFalsy();
-    expect(result.current.error).toBeTruthy();
-    expect(result.current.data).toEqual([]);
+    expect(result.current.isLoading).toBeFalsy();
+    expect(result.current.error).toBeFalsy();
+    expect(result.current.data).toEqual(mockReleaseNotes.releaseNotesData);
+  });
+
+  it('handles api error', async () => {
+    mock.onGet(api).reply(500);
+    const { result, waitForNextUpdate } = renderHook(() => useReleaseNotes());
+    await waitForNextUpdate();
+    expect(result.current.isLoading).toBeTruthy();
+    expect(result.current.isError).toBeFalsy();
+    await waitForNextUpdate();
+    expect(result.current.failureCount).toBe(2);
   });
 });
