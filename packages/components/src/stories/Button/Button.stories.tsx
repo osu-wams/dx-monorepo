92714b3192344427105836a505d0027241527f55
Josh Gum <revgum@gmail.com>
2020-11-10 11:00:39 -0800

    simplify stories to make use of provided decorators

--- packages/components/src/stories/Button/Button.stories.tsx
+++ packages/components/src/stories/Button/Button.stories.tsx
@@ -2,15 +2,6 @@ import React, { ComponentProps } from 'react';
 import { Story } from '@storybook/react/types-6-0';
 import { Button } from 'src/ui/Button';
 import { Color } from 'src/theme/theme-colors';
-import styled from 'styled-components/macro';
-import { spacing } from 'src/theme';
-
-const Container = styled.div`
-  width: 50%;
-  padding: ${spacing.default};
-  display: flex;
-  align-items: center;
-`;
 
 export default {
   title: 'Button/Button',
@@ -27,11 +18,7 @@ export default {
   },
 };
 
-const Template: Story<ComponentProps<typeof Button>> = args => (
-  <Container>
-    <Button {...args}>Click me!</Button>
-  </Container>
-);
+const Template: Story<ComponentProps<typeof Button>> = args => <Button {...args}>Click me!</Button>;
 
 export const Primary = Template.bind({});
 Primary.args = {};
--- packages/components/src/stories/Icon.stories.tsx
+++ packages/components/src/stories/Icon.stories.tsx
@@ -2,16 +2,6 @@ import React, { ComponentProps } from 'react';
 import { Story } from '@storybook/react/types-6-0';
 import Icon from 'src/ui/Icon';
 import { faCoffee } from '@fortawesome/pro-light-svg-icons';
-import styled from 'styled-components/macro';
-import { spacing } from 'src/theme';
-
-const Container = styled.div`
-  width: 50%;
-  padding: ${spacing.default};
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
 
 export default {
   title: 'Icon',
@@ -23,11 +13,7 @@ export default {
   },
 };
 
-const Template: Story<ComponentProps<typeof Icon>> = args => (
-  <Container>
-    <Icon {...args} />
-  </Container>
-);
+const Template: Story<ComponentProps<typeof Icon>> = args => <Icon {...args} />;
 
 export const FontAwesome = Template.bind({});
 FontAwesome.args = { icon: faCoffee, fontSize: '32px' };
@@ -36,12 +22,12 @@ export const FontAwesomeAndCounter = Template.bind({});
 FontAwesomeAndCounter.args = { icon: faCoffee, fontSize: '32px', count: 4, top: true };
 
 const MultiTemplate: Story<ComponentProps<typeof Icon>> = args => (
-  <Container>
+  <>
     <Icon {...args} />
     <Icon {...{ icon: faCoffee, fontSize: '32px', count: 4, top: false }} />
     <Icon {...{ icon: faCoffee, fontSize: '32px', count: 823, top: true }} />
     <Icon {...{ icon: faCoffee, fontSize: '32px', count: undefined, top: false }} />
-  </Container>
+  </>
 );
 export const Samples = MultiTemplate.bind({});
 Samples.args = { icon: faCoffee, fontSize: '32px', count: 4, top: true };
