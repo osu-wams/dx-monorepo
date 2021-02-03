1c37de38294ac38db38ba50779faa321abd242c5
Josh Gum <revgum@gmail.com>
2021-02-03 07:20:13 -0800

    ports grades hook to use react-query

--- packages/hooks/test/api/student/grades.test.ts
+++ packages/hooks/test/api/student/grades.test.ts
@@ -2,24 +2,31 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import { renderHook } from '@testing-library/react-hooks';
 import { useGrades, mockGrades } from '../../../src/api/student/grades';
+import { queryCache } from 'react-query';
 
 const mock = new MockAdapter(axios);
 
+afterEach(() => {
+  queryCache.clear();
+  mock.reset();
+});
+
 describe('useGrades', () => {
   it('gets grades on successful returns', async () => {
-    mock.onGet('/api/student/grades').reply(200, mockGrades.data);
+    mock.onGet('/api/student/grades').replyOnce(200, mockGrades.data);
     const { result, waitForNextUpdate } = renderHook(() => useGrades());
     await waitForNextUpdate();
-    expect(result.current.loading).toBeFalsy();
+    expect(result.current.isLoading).toBeFalsy();
     expect(result.current.error).toBeFalsy();
     expect(result.current.data).toEqual(mockGrades.data);
   });
   it('handles api error', async () => {
-    mock.onGet('/api/student/grades').reply(500);
+    mock.onGet('/api/student/grades').replyOnce(500, '');
     const { result, waitForNextUpdate } = renderHook(() => useGrades());
     await waitForNextUpdate();
-    expect(result.current.loading).toBeFalsy();
-    expect(result.current.error).toBeTruthy();
-    expect(result.current.data).toEqual([]);
+    expect(result.current.isLoading).toBeTruthy();
+    expect(result.current.isError).toBeFalsy();
+    await waitForNextUpdate();
+    expect(result.current.failureCount).toBe(2);
   });
 });
