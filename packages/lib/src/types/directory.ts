25f005ee9d354b25e68cc5986e6fb398c0264c38
Josh Gum <revgum@gmail.com>
2021-02-08 15:56:25 -0800

    fixes types

--- packages/lib/src/types/directory.ts
+++ packages/lib/src/types/directory.ts
@@ -1,26 +1,23 @@
 export interface Directory {
   id: string;
   type: string;
-  attributes: {
-    firstName: string;
-    lastName: string;
-    fullName: string;
-    primaryAffiliation: string;
-    jobTitle: string;
-    department: string;
-    departmentMailingAddress: string;
-    officePhoneNumber?: string;
-    officeAddress?: string;
-    faxNumber?: string;
-    emailAddress: string;
-    username: string;
-    alternatePhoneNumber?: string;
-    osuUid: string;
-  };
-  links: { self: string };
+  firstName: string;
+  lastName: string;
+  fullName: string;
+  primaryAffiliation: string;
+  jobTitle: string;
+  department: string;
+  departmentMailingAddress: string;
+  officePhoneNumber?: string;
+  officeAddress?: string;
+  faxNumber?: string;
+  emailAddress: string;
+  username: string;
+  alternatePhoneNumber?: string;
+  osuUid: string;
 }
 
 export interface DirectoryResponse {
   links: { self: string };
-  data: Directory[];
+  data: { attributes: Directory }[];
 }
--- packages/lib/src/types/location.ts
+++ packages/lib/src/types/location.ts
@@ -1,67 +1,67 @@
 export interface Location {
   id: string;
+  name: string;
+  tags: string[];
   type: string;
-  attributes: {
-    name: string;
-    tags: string[];
-    type: string;
-    abbreviation?: string;
-    geometry?: {
-      type?: string;
-      coordinates?: string[][][];
-    };
-    summary?: string;
-    description?: string;
-    descriptionHTML?: string;
-    address: string;
-    city: string;
-    state: string;
-    zip: string;
-    county?: string;
-    telephone?: string;
-    fax?: string;
-    thumbnails?: string[];
-    images?: string[];
-    departments?: string[];
-    website?: string;
-    sqft?: string;
-    calendar?: string;
-    campus: string;
-    giRestroomCount: number;
-    giRestroomLimit: boolean;
-    giRestroomLocations: string;
-    synonyms?: string[];
-    bldgID?: string;
-    parkingZoneGroup?: string;
-    propID?: string;
-    adaParkingSpaceCount?: number;
-    motorcycleParkingSpaceCount?: number;
-    evParkingSpaceCount?: number;
-    weeklyMenu?: string;
-    adaEntries?: {
-      lon: number;
-      lat: number;
-      accessible: boolean;
-      function: string;
-    }[];
-    aedInventories?: {
-      lon: number;
-      lat: number;
-      location?: string;
-      floor?: number;
-      make?: string;
-      model?: string;
-      serialNo?: string;
-      departmentOwner?: string;
-      contact?: string;
-    }[];
-    latitude: string;
-    longitude: string;
+  abbreviation?: string;
+  geometry?: {
+    type?: string;
+    coordinates?: string[][][];
   };
-  links: { self: string };
+  summary?: string;
+  description: string | null;
+  descriptionHTML: string | null;
+  address: string | null;
+  city: string | null;
+  state: string | null;
+  zip: string | null;
+  county?: string;
+  telephone?: string;
+  fax?: string;
+  thumbnails?: string[];
+  image: string | null; // Derived from thumbnails property
+  images?: string[];
+  departments?: string[];
+  website?: string;
+  link: string | null; // Derived from the website property
+  sqft?: string;
+  calendar?: string;
+  campus: string;
+  giRestroomCount: number;
+  giRestroomLimit: boolean;
+  giRestroomLocations: string;
+  synonyms?: string[];
+  bldgID?: string;
+  parkingZoneGroup?: string;
+  propID?: string;
+  adaParkingSpaceCount?: number;
+  motorcycleParkingSpaceCount?: number;
+  evParkingSpaceCount?: number;
+  weeklyMenu?: string;
+  adaEntries?: {
+    lon: number;
+    lat: number;
+    accessible: boolean;
+    function: string;
+  }[];
+  aedInventories?: {
+    lon: number;
+    lat: number;
+    location?: string;
+    floor?: number;
+    make?: string;
+    model?: string;
+    serialNo?: string;
+    departmentOwner?: string;
+    contact?: string;
+  }[];
+  latitude: string;
+  longitude: string;
 }
 
 export interface LocationResponse {
   links: { self: string };
-  data: Location[];
+  data: {
+    attributes: Location;
+  }[];
 }
