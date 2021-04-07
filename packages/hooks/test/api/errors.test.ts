edf2d578527830d25ca68e796020d289286c47ee
Josh Gum <revgum@gmail.com>
2021-04-07 09:47:52 -0700

    adds api call for posting application messages to the server

--- packages/hooks/test/api/errors.test.ts
+++ packages/hooks/test/api/errors.test.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { postError } from '../../src/api/errors';
+import { postError, postAppMessageError } from '../../src/api/errors';
 
 const mock = new MockAdapter(axios);
 
@@ -13,6 +13,31 @@ describe('postError', () => {
   it('logs an error when posting fails', async () => {
     mock.onPost('/api/errors').networkError();
     const result = await postError(new Error('test'));
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('postAppMessageError', () => {
+  it('returns successful', async () => {
+    mock.onPost('/api/errors/app-message').reply(200);
+    const result = await postAppMessageError({
+      body: 'Message Body',
+      title: 'Message Title',
+      type: 'error',
+      visible: true,
+      id: 'id123',
+    });
+    expect(result).toBeUndefined();
+  });
+  it('logs an error when posting fails', async () => {
+    mock.onPost('/api/errors/app-message').networkError();
+    const result = await postAppMessageError({
+      body: 'Message Body',
+      title: 'Message Title',
+      type: 'error',
+      visible: true,
+      id: 'id123',
+    });
     expect(result).toBeUndefined();
   });
 });
