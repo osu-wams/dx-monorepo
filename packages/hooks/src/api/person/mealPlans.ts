f0a2d3e0037f12e133fc765a2e8ade2268a5fa33
Josh Gum <revgum@gmail.com>
2021-05-18 09:41:46 -0700

    fixes response type for meal plans endpoint

--- packages/hooks/src/api/person/mealPlans.ts
+++ packages/hooks/src/api/person/mealPlans.ts
@@ -5,5 +5,5 @@ import { REACT_QUERY_DEFAULT_CONFIG } from '../../constants';
 
 export const mockMealPlans = mocks;
 
-export const useMealPlans = (opts: UseQueryOptions<Types.PersonsAttributes, Error> = REACT_QUERY_DEFAULT_CONFIG) =>
+export const useMealPlans = (opts: UseQueryOptions<Types.MealPlan[], Error> = REACT_QUERY_DEFAULT_CONFIG) =>
   useQuery('/api/persons/meal-plans', opts);
