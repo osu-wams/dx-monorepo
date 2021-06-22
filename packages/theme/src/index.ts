5145feaf920e388b05ff4981ffd4e066e27c458c
Josh Gum <revgum@gmail.com>
2021-06-22 07:40:44 -0700

    removes global styles from theme package

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,7 +1,6 @@
-import GlobalStyles from './GlobalStyles';
 import { light } from './themes';
 export { shadows, gradients, breakpoints, spacing, fontSize, borderRadius, mq } from './theme-settings';
 export { Color } from './theme-colors';
 export { dark, themesLookup, defaultTheme } from './themes';
 export type MOSTheme = typeof light;
-export { GlobalStyles, light };
+export { light };
